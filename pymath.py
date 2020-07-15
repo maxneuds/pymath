a512737764281867907643e0b259f64e2775d6f5
Maximilian Neudert <max@neuds.de>
2020-07-15 23:18:03 +0200

    init

---
+++ pymath.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from numpy import log as log
+from numpy import log2 as log2
+from numpy import exp as exp
+from numpy import sum as sum
+from scipy.special import binom as binom
